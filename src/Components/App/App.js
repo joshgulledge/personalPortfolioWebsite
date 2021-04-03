aaf3cb34e51c1bd096b42704b00b8ef55d68178e
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-04-03 15:23:20 -0500

    kinda got an idea now

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import NavBar from '../Nav/Nav';
+import './App.css';
+
+import winterHike from '../../Images/winterHike.jpg';
+
+// material ui
+import {ThemeProvider, createMuiTheme, makeStyles} from '@material-ui/core/styles';
+import { Grid, Paper} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    width: '50%',
+    margin: '20px',
+    spacing: (2)
+  },
+}));
+const newTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      // light: '',
+      main: '#373E40',
+      // dark: '',
+      contrastText: '#B7D5D4',
+    },
+    secondary: {
+      main: '#77878B',
+    },
+  }
+}); // end newTheme
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <NavBar />
-    </div>
+    <ThemeProvider theme={newTheme}>
+      <Grid container>
+        <Grid item container xs={12}>
+          <NavBar />
+        </Grid>
+        
+        <Grid item xs={5} >
+          <Paper className={classes.root} elevation={3}>
+            <img src={winterHike} style={{borderRadius: '5px'}} alt='winter profile' width='100%'/>
+          </Paper>
+        </Grid>
+        <Grid item xs={2} />
+
+        <Grid item xs={5}>
+          <Paper style={{backgroundColor: '#acb0b1', color: 'black'}} className={classes.root} elevation={3}>
+            <p>About me</p>
+          </Paper>
+        </Grid>
+        
+      
+        
+      </Grid>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
+// material ui stuff
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
@@ -19,19 +17,55 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
+
 const NavBar = function () {
   const classes = useStyles();
 
+  // functions
+  const projectButton = function () {
+    // go to project page
+    console.log('Project button clicked');
+  }; // end projectButton
+
+  const employmentButton = function () {
+    console.log('Employment button clicked');
+  }; // end employmentButton
+
+  const educationButton = function () {
+    console.log('Education button clicked');
+  }; // end educationButton
+
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h3" className={classes.title}>
-          Joshua Gulledge
-        </Typography>
-        <Button color="inherit">Projects</Button>
-        <Button color="inherit">Employment</Button>
-        <Button color="inherit">Education</Button>
-      </Toolbar>
+      {/* <Grid container item xs={12}> */}
+        <Toolbar>
+          <Grid item xs={12} className={classes.root} spacing={2} container justify='center' alignItems='flex-end'>
+
+            <Grid item xs={6}>
+                <Typography variant="h3" className={classes.title}>
+                  Joshua Gulledge
+                </Typography>
+                <Typography variant="body2" className={classes.title}>
+                  Full Stack Software Engineer
+                </Typography>
+            </Grid>
+
+            <Grid item xs={2}>
+              <Button onClick={projectButton} color="inherit">Projects</Button>
+            </Grid>
+
+            <Grid item xs={2}>
+              <Button onClick={employmentButton} color="inherit">Employment</Button>
+            </Grid>
+
+            <Grid item xs={2}>
+              <Button onClick={educationButton} color="inherit">Education</Button>
+            </Grid>
+            
+          </Grid>
+        </Toolbar>
+      {/* </Grid> */}
+
     </AppBar>
   )
 }; //end NavBar
